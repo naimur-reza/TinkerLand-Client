4c12c93134db0caa2434bd080493665525b312e6
Anik <naimur.rezaa@gmail.com>
2023-05-17 22:23:19 +0600

    added home route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,8 @@
 const { createBrowserRouter } = require("react-router-dom");
 const { default: LoginLayout } = require("../layout/LoginLayout");
 import { useNavigate, Navigate } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +20,16 @@ const router = createBrowserRouter([
       {
         path: "/register",
         element: <Navigate to={"/home"} />,
+      },
+    ],
+  },
+  {
+    path: "home",
+    element: <Main />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
       },
     ],
   },
