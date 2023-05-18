23bd437cc5c154907600a7fb489d4e2cbe4aca8c
Anik <naimur.rezaa@gmail.com>
2023-05-18 18:51:37 +0600

    done with react hook form in add toy

--- src/pages/AddToys.jsx
+++ src/pages/AddToys.jsx
@@ -1,7 +1,16 @@
-import { Input } from "@material-tailwind/react";
 import React from "react";
+import { Button, Input } from "@material-tailwind/react";
+import { useForm } from "react-hook-form";
 
 const AddToys = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="my-container">
       <h1 className="text-center pt-5  text-2xl w-[500px] tracking-wide  mx-auto">
@@ -13,23 +22,56 @@ const AddToys = () => {
         and parents. Discover a diverse range of options, from action figures to
         puzzles and plush toys to board games
       </p>
-      <form className="space-y-5">
+      <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex gap-5 ">
-          <Input color="cyan" size="lg" label="Image URL" />
-          <Input color="cyan" size="lg" label="Seller Name" />
+          <Input
+            color="cyan"
+            size="lg"
+            label="Image URL"
+            {...register("imageURl")}
+          />
+          <Input
+            color="cyan"
+            size="lg"
+            label="Seller Name"
+            {...register("seller_name")}
+          />
         </div>
         <div className="flex gap-5">
-          <Input color="cyan" size="lg" label="Email" />
-          <Input color="cyan" size="lg" label="Sub Category" />
+          <Input color="cyan" size="lg" label="Email" {...register("email")} />
+          <Input
+            color="cyan"
+            size="lg"
+            label="Sub Category"
+            {...register("sub_category")}
+          />
         </div>
         <div className="flex gap-5">
-          <Input color="cyan" size="lg" label="Price" />
-          <Input color="cyan" size="lg" label="Rating" />
+          <Input color="cyan" size="lg" label="Price" {...register("price")} />
+          <Input
+            color="cyan"
+            size="lg"
+            label="Rating"
+            {...register("rating")}
+          />
         </div>
         <div className="flex gap-5">
-          <Input color="cyan" size="lg" label="Available Quantity" />
-          <Input color="cyan" size="lg" label="Detail Description" />
+          <Input
+            color="cyan"
+            size="lg"
+            label="Available Quantity"
+            {...register("quantity")}
+          />
+          <Input
+            color="cyan"
+            size="lg"
+            label="Detail Description"
+            {...register("description")}
+          />
         </div>
+        <Button fullWidth type="submit">
+          Add This Toy
+        </Button>
       </form>
     </div>
   );
