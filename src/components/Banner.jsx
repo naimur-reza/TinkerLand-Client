f9c9a7e7cb5bcca573be92393dac33f44983ee9c
Anik <naimur.rezaa@gmail.com>
2023-05-18 16:44:21 +0600

    added banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,37 +1,110 @@
-import { Carousel } from "@material-tailwind/react";
+import { Carousel, Typography, Button } from "@material-tailwind/react";
 
 export default function Banner() {
   return (
-    <Carousel
-      className=" h-[100vh]"
-      navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2 ">
-          {new Array(length).fill("").map((_, i) => (
-            <span
-              key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                activeIndex === i ? "bg-white w-8" : "bg-white/50 w-4"
-              }`}
-              onClick={() => setActiveIndex(i)}
-            />
-          ))}
+    <Carousel className="h-[100vh]">
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/0F3Rjjs/chris-hardy-H5-Ffv4-I5-ZMI-unsplash.jpg"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/50">
+          <div className="w-3/4 text-center md:w-2/4">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl">
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80">
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+            <div className="flex justify-center gap-2">
+              <Button size="lg" color="white">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
         </div>
-      )}>
-      <img
-        src="https://i.ibb.co/0F3Rjjs/chris-hardy-H5-Ffv4-I5-ZMI-unsplash.jpg"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        alt="image 2"
-        src="https://i.ibb.co/kDYhGQ8/mateusz-waclawek-t2b2sv-Mf8ek-unsplash.jpg"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://i.ibb.co/ZzJy0SW/rachit-khurana-V3gb-V-ke-H10-unsplash.jpg"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
+      </div>
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/kDYhGQ8/mateusz-waclawek-t2b2sv-Mf8ek-unsplash.jpg"
+          alt="image 2"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full items-center bg-black/50">
+          <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl">
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80">
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+            <div className="flex gap-2">
+              <Button size="lg" color="white">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/ZzJy0SW/rachit-khurana-V3gb-V-ke-H10-unsplash.jpg"
+          alt="image 3"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full items-end bg-black/50">
+          <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl">
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80">
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+            <div className="flex gap-2">
+              <Button size="lg" color="white">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Carousel>
   );
 }
