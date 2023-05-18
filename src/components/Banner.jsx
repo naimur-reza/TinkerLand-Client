459dc9e4d1d67b432348f1eb0ddd80725e50c8f7
Anik <naimur.rezaa@gmail.com>
2023-05-18 17:09:45 +0600

    modified banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -105,6 +105,40 @@ export default function Banner() {
           </div>
         </div>
       </div>
+      <div className="relative h-full w-full">
+        <img
+          src="https://i.ibb.co/fr63bZW/freestocks-CH6-cfi-Y2-Yo-unsplash.jpg"
+          alt="image 3"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 grid h-full w-full items-end bg-black/50">
+          <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4 text-3xl md:text-4xl lg:text-5xl">
+              The Beauty of Nature
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-12 opacity-80">
+              It is not so much for its beauty that the forest makes a claim
+              upon men&apos;s hearts, as for that subtle something, that quality
+              of air that emanation from old trees, that so wonderfully changes
+              and renews a weary spirit.
+            </Typography>
+            <div className="flex gap-2">
+              <Button size="lg" color="white">
+                Explore
+              </Button>
+              <Button size="lg" color="white" variant="text">
+                Gallery
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Carousel>
   );
 }
