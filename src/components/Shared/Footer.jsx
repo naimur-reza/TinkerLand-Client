8d3db8afd236cf500e40e4bd97e56d2256b3f756
Anik <naimur.rezaa@gmail.com>
2023-05-18 19:54:42 +0600

    done with add toy design

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@material-tailwind/react";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-white p-8">
+    <footer className="w-full bg-white p-8 ">
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
         <img src="/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
         <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
--- src/pages/AddToys.jsx
+++ src/pages/AddToys.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Input } from "@material-tailwind/react";
 import { useForm } from "react-hook-form";
-
+import "../../src/App.css";
 const AddToys = () => {
   const {
     register,
@@ -12,67 +12,84 @@ const AddToys = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="my-container">
-      <h1 className="text-center pt-5  text-2xl w-[500px] tracking-wide  mx-auto">
-        Add Toys: Expand Your Collection with Exciting New Additions
-      </h1>
-      <p className="px-20 text-center tracking-wide py-3 text-gray-700">
-        Enhance your toy collection by adding new and exciting toys. Our 'Add
-        Toys' page is the perfect destination for toy enthusiasts, collectors,
-        and parents. Discover a diverse range of options, from action figures to
-        puzzles and plush toys to board games
-      </p>
-      <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex gap-5 ">
-          <Input
-            color="cyan"
-            size="lg"
-            label="Image URL"
-            {...register("imageURl")}
-          />
-          <Input
-            color="cyan"
-            size="lg"
-            label="Seller Name"
-            {...register("seller_name")}
-          />
-        </div>
-        <div className="flex gap-5">
-          <Input color="cyan" size="lg" label="Email" {...register("email")} />
-          <Input
-            color="cyan"
-            size="lg"
-            label="Sub Category"
-            {...register("sub_category")}
-          />
-        </div>
-        <div className="flex gap-5">
-          <Input color="cyan" size="lg" label="Price" {...register("price")} />
-          <Input
-            color="cyan"
-            size="lg"
-            label="Rating"
-            {...register("rating")}
-          />
-        </div>
-        <div className="flex gap-5">
-          <Input
-            color="cyan"
-            size="lg"
-            label="Available Quantity"
-            {...register("quantity")}
-          />
-          <Input
-            color="cyan"
-            size="lg"
-            label="Detail Description"
-            {...register("description")}
-          />
-        </div>
-        <Button fullWidth type="submit">
-          Add This Toy
-        </Button>
-      </form>
+    <div className="bg-cover  transformer min-h-[calc(100vh-64px)] ">
+      <div className="my-container ">
+        <h1 className="text-center text-gray-200 pt-5  text-2xl w-[500px] tracking-wide  mx-auto">
+          Add Toys: Expand Your Collection with Exciting New Additions
+        </h1>
+        <p className="px-20 text-center tracking-wide py-3 text-gray-400">
+          Enhance your toy collection by adding new and exciting toys. Our 'Add
+          Toys' page is the perfect destination for toy enthusiasts, collectors,
+          and parents. Discover a diverse range of options, from action figures
+          to puzzles and plush toys to board games
+        </p>
+        <form className="space-y-5 pt-10" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex gap-5 ">
+            <Input
+              color="white"
+              size="lg"
+              className="text"
+              label="Image URL"
+              {...register("imageURl")}
+            />
+            <Input
+              color="white"
+              size="lg"
+              label="Seller Name"
+              {...register("seller_name")}
+            />
+          </div>
+          <div className="flex gap-5">
+            <Input
+              color="white"
+              size="lg"
+              label="Email"
+              {...register("email")}
+            />
+            <Input
+              color="white"
+              size="lg"
+              label="Sub Category"
+              {...register("sub_category")}
+            />
+          </div>
+          <div className="flex gap-5">
+            <Input
+              color="white"
+              size="lg"
+              label="Price"
+              {...register("price")}
+            />
+            <Input
+              color="white"
+              size="lg"
+              label="Rating"
+              {...register("rating")}
+            />
+          </div>
+          <div className="flex gap-5">
+            <Input
+              color="white"
+              size="lg"
+              label="Available Quantity"
+              {...register("quantity")}
+            />
+            <Input
+              color="white"
+              size="lg"
+              label="Detail Description"
+              {...register("description")}
+            />
+          </div>
+          <Button
+            variant="gradient"
+            color="deep-purple"
+            fullWidth
+            type="submit">
+            Add This Toy
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
