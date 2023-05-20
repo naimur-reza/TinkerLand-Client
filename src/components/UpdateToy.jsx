0f7dc0088d926f8754926529d1a87e92d5295b87
Anik <naimur.rezaa@gmail.com>
2023-05-20 09:42:49 +0600

    done with footer design

--- src/components/UpdateToy.jsx
+++ src/components/UpdateToy.jsx
@@ -5,34 +5,107 @@ import {
   DialogHeader,
   DialogBody,
   DialogFooter,
+  Input,
 } from "@material-tailwind/react";
 
-export default function UpdateToy({ open, handleOpen }) {
-  //   const [open, setOpen] = useState(false);
-  //   const handleOpen = () => setOpen(!open);
+import { toast } from "react-hot-toast";
+import { useForm } from "react-hook-form";
 
+export default function UpdateToy({ open, handleOpen }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    // fetch("http://localhost:5000/toys", {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data));
+    toast.success("Done");
+  };
   return (
     <Fragment>
       <Dialog open={open} handler={handleOpen}>
-        <DialogHeader>Its a simple dialog.</DialogHeader>
+        <DialogHeader>Update Your Toy.</DialogHeader>
         <DialogBody divider>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ad
-          reprehenderit omnis perspiciatis aut odit! Unde architecto
-          perspiciatis, dolorum dolorem iure quia saepe autem accusamus eum
-          praesentium magni corrupti explicabo!
+          <form className="space-y-5 pt-10" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex gap-5 ">
+              <Input
+                color="cyan"
+                size="lg"
+                className="text"
+                label="Name"
+                {...register("name")}
+              />
+              <Input
+                color="cyan"
+                size="lg"
+                label="Sub Category"
+                {...register("sub_category")}
+              />
+            </div>
+
+            <div className="flex gap-5">
+              <Input
+                color="cyan"
+                size="lg"
+                label="Price"
+                {...register("price")}
+              />
+              <Input
+                color="cyan"
+                size="lg"
+                label="Rating"
+                {...register("rating")}
+              />
+            </div>
+            <div className="flex gap-5">
+              <Input
+                color="cyan"
+                size="lg"
+                label="Available Quantity"
+                {...register("quantity")}
+              />
+              <Input
+                color="cyan"
+                size="lg"
+                label="Detail Description"
+                {...register("description")}
+              />
+            </div>
+            <Input
+              color="cyan"
+              size="lg"
+              className="text"
+              label="Image URL"
+              {...register("imageURl")}
+            />
+            <hr />
+            <DialogFooter>
+              <Button
+                variant="text"
+                color="red"
+                onClick={handleOpen}
+                className="mr-1">
+                <span>Cancel</span>
+              </Button>
+              <Button
+                type="submit"
+                variant="gradient"
+                color="cyan"
+                onClick={handleOpen}>
+                <span>Update</span>
+              </Button>
+            </DialogFooter>
+          </form>
         </DialogBody>
-        <DialogFooter>
-          <Button
-            variant="text"
-            color="red"
-            onClick={handleOpen}
-            className="mr-1">
-            <span>Cancel</span>
-          </Button>
-          <Button variant="gradient" color="green" onClick={handleOpen}>
-            <span>Confirm</span>
-          </Button>
-        </DialogFooter>
       </Dialog>
     </Fragment>
   );
